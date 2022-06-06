564946c140fe826e18bfb8c05b1cbfffed20f28a
jonas da cunha madeira <jonascn.2021@gmail.com>
2022-06-06 14:38:14 -0300

    node

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+
+
+import express from 'express'
+import cors from 'cors'
+
+const server = express 
+server.use(cors());
+server.use(express());
+
+server.listen(process.env.PORT ,
+    () => console.log(`API Online na porta ${process.env.PORT}`) )
---
+++ api/src/repository/connection.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise'
+
+const con = await mysql.connection({
+
+    HOST=process.env,
+    USER=mysql_,
+    PWD=1234
+
+})
+
+
+export{con}
